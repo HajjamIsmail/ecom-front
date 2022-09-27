cc5699db8e3e14339753ccc21197c52c37dc27a8
HAJJAM ISMAIL <hajjam.ism@gmail.com>
2022-09-27 17:08:29 +0100

    commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CaddyComponent } from './caddy/caddy.component';
+import { ClientComponent } from './client/client.component';
+import { LoginComponent } from './login/login.component';
+import { PaymentComponent } from './payment/payment.component';
+import { ProductComponent } from './product/product.component';
+import { ProductsComponent } from './products/products.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CaddyComponent,
+    ClientComponent,
+    LoginComponent,
+    PaymentComponent,
+    ProductComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
